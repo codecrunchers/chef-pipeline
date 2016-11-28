822a3e7826b673d2f5f17acfd3322fef3cd624e5
Alan <alan@codecrunchers.ie>
2016-11-28 21:37:39 +0000

    gitlab working

--- .chef/client.rb
+++
@@ -1,2 +0,0 @@
-cookbook_path ['/home/alan/devel/chef-repo/cookbooks/']
-file_cache_path '/home/alan/devel/chef-repo/.chef/local-mode-cache'
--- .chef/local-mode-cache/cache/cookbooks/rxp_pipeline/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['pipeline']['services'] = [ 'http://localhost:18080','http://localhost:19000','http://localhost:10080']
-default['pipeline']['docker_file'] =  '/home/alan/devel/docker/docker-pipeline/docker-ci-tool-stack/docker-compose.yml'
-default['pipeline']['gitlab_url'] =  'http://localhost:10080/api/v3'
-
-
-
