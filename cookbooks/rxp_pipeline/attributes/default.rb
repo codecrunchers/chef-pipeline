4fc9235bd8daf49df3af74591aed1737d4cf53a4
Alan <alan@codecrunchers.ie>
2016-11-28 23:16:26 +0000

    tidy up

--- cookbooks/rxp_pipeline/attributes/default.rb
+++ cookbooks/rxp_pipeline/attributes/default.rb
@@ -1,6 +1,7 @@
 default['pipeline']['services'] = [ 'http://localhost:18080','http://localhost:19000','http://localhost:10080']
 default['pipeline']['docker_file'] =  '/home/alan/devel/docker/docker-pipeline/docker-ci-tool-stack/docker-compose.yml'
 default['pipeline']['gitlab_url'] =  'http://localhost:10080/api/v3'
+default['pipeline']['user'] =  [{:name=>"Alan",:sshkey=>"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ouczXf5V+/HsOwYIYClV9Kgklb58ILuuc3vM0cJFRbj+CbsBhDLioWdeDCfmK1VQHIAucO/QXfuDTfVmtM8g/kmGs4ifwhcxHJMTrNcTAL9MHSzi7lClwU/slSAzusxVFcAB2qd0ncenY14+gBK21WYnjFYISsblklZYvVZkvhn2A259iMIW+GPFKz10Ykeq/Q3bxG55BCJ0B22smZr9vA1OMyHAPOA7vdWfs6NiGnWqL3s0Npk5e0XSZMfhAmEFhB3r+Qx0y5r1+/SAgUXkh3vTH9bj7/Z7k11EQo+PLz6lPxeikrR2fZ5301x6b9Nf6y0ejqKZYrgXOCJK7TaZ alan@office.localdomain"}]
 
 
 
--- cookbooks/rxp_pipeline/resources/gitlab.rb
+++ cookbooks/rxp_pipeline/resources/gitlab.rb
@@ -1,14 +1,20 @@
 require 'gitlab'
+require 'pp'
 require 'securerandom'
 
 
 property :host, String
 property :post_data,  :kind_of => Hash
 
-
+##
+# Get a token, 
+# create a project, 
+# add members to new group, 
+# assign group to project
+##
 action :setup do
   Chef::Log.debug("Calling (POST) %{host}")
-  response = doPost(host,post_data)
+  response = doHttpPost(host,post_data)
   token = response['private_token']
   proj_name=SecureRandom.hex
 
@@ -18,23 +24,59 @@ action :setup do
   end
 
   begin
-    group=Gitlab.client.create_group(proj_name,proj_name,{})
-    project=Gitlab.client.create_project(proj_name,{})
-    [{:name=>proj_name,:key=>"adasd"}].each do |user|
+    group=create_group(proj_name,{})
+    project=create_project(proj_name,{})
+
+    node["pipeline"]["user"].each do |user|
       Chef::Log.debug("Creating #{user[:name]} for #{proj_name}")
+
       email="#{proj_name}@x.org".to_s
-      userObj=Gitlab.client.create_user(email, 'password', "#{user[:name]}", { name: 'Demo User' })
-      Gitlab.client.add_group_member(group.id, userObj.id, 40)
-      Gitlab.client.transfer_project_to_group(group.id,project.id)
+      userObj=create_user(email, 'password', SecureRandom.hex, { name: "#{SecureRandom.hex} Demo User" })
+      add_user_to_group(userObj,group)
+      add_user_ssh_key(user)
+      assign_project_to_group(project,group)
     end
-    Chef::Log.debug("Group #{group.id}")
   rescue
-    Chef::Log.debug("Move on #{$!}")
+    Chef::Log.error("Cannot Complete Gitlab Init #{$!}")
   end
 
 end
 
-def doPost(_host,_post_data)
+def create_group(proj_name,options = {})
+  group =  Gitlab.client.create_group(proj_name,proj_name,options)
+  PP.pp(group)
+#  Chef::Log.debug("Group #{PP.pp(group)}")
+  return group
+end
+
+def create_project(proj_name,options={})
+  proj =  Gitlab.client.create_project(proj_name,options)
+  PP.pp(proj)
+  return proj
+
+end
+
+def create_user(email,password,username,jsonAttrs)
+  user = Gitlab.client.create_user(email, password, "Username: #{username}" , jsonAttrs)
+  PP.pp(user)
+  return user
+end
+
+def add_user_to_group(user,group)
+  Gitlab.client.add_group_member(group.id, user.id, 40)
+end
+
+def assign_project_to_group(project,group)
+  Gitlab.client.transfer_project_to_group(group.id,project.id)
+end
+
+def add_user_ssh_key(user)
+  key = Gitlab.client.create_ssh_key("#{user.name}","#{user.sshkey}")
+  PP.pp(key)
+end
+
+
+def doHttpPost(_host,_post_data)
   url = _host
   uri = URI(url)
   Chef::Log.debug(uri.path)
